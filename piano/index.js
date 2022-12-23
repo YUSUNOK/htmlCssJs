cec74571db8ab692c0bd33f2bd51cae9ef645bb3
Doyun Lee <sunokryu319@gmail.com>
2022-12-23 23:49:58 +0900

    piano 12/23

--- piano/index.js
+++ piano/index.js
@@ -1,14 +1,19 @@
 const keyList = document.querySelectorAll(".event");
 for (let i = 0; i < keyList.length; i++) {
   keyList[i].addEventListener("click", function () {
-    console.log(keyList[i]);
-    keyList[i].style.background = "red";
+    const list = document.getElementsByClassName(i);
+    for (let j = 0; j < list.length; j++) {
+      list[j].style.background = "red";
+    }
     var audio = new Audio("audio/" + i + ".mp3");
     audio.load();
     audio.volume = 1;
     audio.play();
     setTimeout(function () {
       audio.pause();
+      for (let j = 0; j < list.length; j++) {
+        list[j].style.background = "white";
+      }
     }, 300);
   });
 }
@@ -16,12 +21,21 @@ for (let i = 0; i < keyList.length; i++) {
 // 키보드 0123456이 각각 도레미파솔라시에 대응되게
 document.addEventListener("keydown", (event) => {
   const Key = event.code;
+
   const index = Key.charAt(Key.length - 1);
+  const list = document.getElementsByClassName(index);
+  for (let i = 0; i < list.length; i++) {
+    list[i].style.background = "red";
+  }
+
   var audio = new Audio("audio/" + index + ".mp3");
   audio.load();
   audio.volume = 1;
   audio.play();
   setTimeout(function () {
     audio.pause();
+    for (let i = 0; i < list.length; i++) {
+      list[i].style.background = "white";
+    }
   }, 300);
 });
